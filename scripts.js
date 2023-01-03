d4a211bd64d718648764b150c3ec323c2d9cb3f2
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-01-03 20:08:59 +0200

    Disable-menu-scroll-js

--- scripts.js
+++ scripts.js
@@ -5,29 +5,42 @@ const mBtnHello = document.getElementById('mBtnHello');
 const mBtnPortfolio = document.getElementById('mBtnPortfolio');
 const mBtnAbout = document.getElementById('mBtnAbout');
 const mBtnContact = document.getElementById('mBtnContact');
+const bodyVis = document.getElementById('ani');
 
 mBtnHello.addEventListener('click', () => {
   menuCo.style.display = 'none';
+  bodyVis.style.position = 'static';
+  bodyVis.style.overflow = 'auto';
 });
 
 mBtnPortfolio.addEventListener('click', () => {
   menuCo.style.display = 'none';
+  bodyVis.style.position = 'static';
+  bodyVis.style.overflow = 'auto';
 });
 
 mBtnAbout.addEventListener('click', () => {
   menuCo.style.display = 'none';
+  bodyVis.style.position = 'static';
+  bodyVis.style.overflow = 'auto';
 });
 
 mBtnContact.addEventListener('click', () => {
   menuCo.style.display = 'none';
+  bodyVis.style.position = 'static';
+  bodyVis.style.overflow = 'auto';
 });
 
 closeMenu.addEventListener('click', () => {
   menuCo.style.display = 'none';
+  bodyVis.style.position = 'static';
+  bodyVis.style.overflow = 'auto';
 });
 
 menuBtn.addEventListener('click', () => {
   menuCo.style.display = 'block';
+  bodyVis.style.position = 'sticky';
+  bodyVis.style.overflow = 'hidden';
 });
 
 const mediaQuery = window.matchMedia('(min-width: 768px)');
@@ -35,6 +48,8 @@ const mediaQuery = window.matchMedia('(min-width: 768px)');
 function screenSize(i) {
   if (i.matches) {
     menuCo.style.display = 'none';
+    bodyVis.style.position = 'static';
+    bodyVis.style.overflow = 'auto';
   }
 }
 mediaQuery.addListener(screenSize);
